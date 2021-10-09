219b36f39a6a3f4d253353352dce7aab4fe66479
marcoalonso <marcoalonsorota@hotmail.com>
2021-10-09 13:12:47 -0500

    Saving-Personal-Notes

---
+++ EASYNOTES/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  EASYNOTES
+//
+//  Created by marco rodriguez on 09/10/21.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
---
+++ EASYNOTES/View/ContentView.swift
@@ -0,0 +1,125 @@
+//
+//  ContentView.swift
+//  EASYNOTES
+//
+//  Created by marco rodriguez on 09/10/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    // MARK: - Property
+    @State var task : String = ""
+    
+    private var isButtonDisabled: Bool {
+        task.isEmpty
+    }
+    
+    // MARK: - Fetching Data
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    // MARK: - Function
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
+            
+            do {
+                try viewContext.save()
+            } catch {
+                
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+            task = ""
+            hideKeyboard()
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    // MARK: - Body
+    var body: some View {
+        NavigationView {
+            VStack{
+                VStack(spacing: 16) {
+                  TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(UIColor.systemGray6)
+                        )
+                        .cornerRadius(10)
+                    Button(action: {
+                        addItem()
+                    }, label: {
+                        Spacer()
+                        Text("SAVE")
+                        Spacer()
+                    })
+                    
+                        .disabled(isButtonDisabled)
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(isButtonDisabled ? Color.gray : Color.pink)
+                        .cornerRadius(12)
+                }//: Vstack
+                .padding()
+                
+                List {
+                    ForEach(items) { item in
+                        VStack(alignment: .leading) {
+                            Text(item.task ?? "")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            
+                            Text("Item at: \(item.timestamp!, formatter: itemFormatter)")
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }//: Vstack
+                    }
+                    .onDelete(perform: deleteItems)
+                }//: List
+            }//: VStack
+            .navigationBarTitle("Daily Tasks", displayMode: .large)
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                #endif
+            }//: .toolbar
+        }//: Nav
+    }
+
+    
+}
+
+// MARK: - Preview
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
